1f7e875f195c22b1e2ee05ec5cacc536d8e9dd39
Andivyy11 <andivyy@gmail.com>
2024-09-19 20:02:41 +0530

    done

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami' , (req,res)=>{
+  res.json({
+ ipaddress : req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+language:req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0] : 'unknown',
+software : req.headers['user-agent'] || 'unknown'
+  })
+})
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
